6a67b2f239a2b7f0896cfcb403d0dce9bf3d101d
Liuyong Ding <ly_ding@126.com>
2020-03-20 09:10:15 +0000

    version 0.0.5

---
+++ R/get_province.R
@@ -0,0 +1,21 @@
+#' @title Statistics on Species or Infraspecies by Province
+#' @description Download the statistics datasets on species/infraspecies by province at \url{http://sp2000.org.cn/statistics/statistics_map} for more details.
+#' @rdname  get_province
+#' @name get_province
+#' @return Statistics on species or infraspecies by province.
+#' @author Liuyong Ding
+#' @details Visit the website \url{http://sp2000.org.cn/statistics/statistics_map} for more details.
+#' @importFrom jsonlite fromJSON
+#' @examples
+#' \donttest{
+#' x <- get_province()
+#' head(x)
+#' }
+#' @export
+get_province <- function() {
+  url <- 'http://sp2000.org.cn/statistics/show_in_map_all'
+  map_all <- fromJSON(url)
+  names(map_all$data) <- c("province", "species_counts")
+  map_all$date <- as.Date(Sys.time())
+  return(map_all)
+}
